57dd28e6eaf526ec9fade06aba40a96a43a01ebf
Shivam Bansal <shivam2297@gmail.com>
2019-04-09 10:33:22 +0530

    Added second activity

--- app/src/main/java/com/example/shivam/memoryleaksdemo/MainActivity.java
+++ app/src/main/java/com/example/shivam/memoryleaksdemo/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.shivam.memoryleaksdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private Button mPressMeBtn;
+    private boolean clickable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Main Activity");
+
+        mTextView = findViewById(R.id.textview);
+        mPressMeBtn = findViewById(R.id.press_me_btn);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        clickable = true;
+
+        mPressMeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!clickable)
+                    return;
+                else {
+                    clickable = false;
+                    Intent secondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(secondActivityIntent);
+                }
+            }
+        });
     }
 }
